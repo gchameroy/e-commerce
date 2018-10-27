2ba310fd252048350ae915ca9b9194c76a8ce2b5
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2018-10-27 11:36:46 +0200

    ECMM-8 (Display sub categories on homepage)

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -3,19 +3,16 @@
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\SimpleCache\CacheInterface;
 
 abstract class AbstractService implements ServiceInterface
 {
-    private $cache;
     private $em;
     private $entityName;
     private $repository;
     private $cacheKey;
 
-    public function __construct(CacheInterface $cache, EntityManagerInterface $em, string $entityName, string $cacheKey)
+    public function __construct(EntityManagerInterface $em, string $entityName, string $cacheKey)
     {
-        $this->cache = $cache;
         $this->em = $em;
         $this->entityName = $entityName;
         $this->repository = $em->getRepository($entityName);
@@ -24,12 +21,6 @@ abstract class AbstractService implements ServiceInterface
 
     public function getList(): array
     {
-        $cacheKey = $this->cacheKey . '.list';
-        $entities = $this->cache->get($cacheKey, $this->repository->findAll());
-        if (!$this->cache->has($cacheKey)) {
-            $this->cache->set($cacheKey, $entities);
-        }
-
-        return $entities;
+        return $this->repository->findAll();
     }
 }
