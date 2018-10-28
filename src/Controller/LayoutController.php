76427bc11d88ec2b89e56a962efb3b766fdb2d9b
Geoffrey Chameroy <geoffrey.chameroy@gmail.com>
2018-10-28 18:38:12 +0100

    ECMM-9 (Update navbar) (#7)

--- src/Controller/LayoutController.php
+++ src/Controller/LayoutController.php
@@ -7,13 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LayoutController extends Controller
 {
-    public function navbar(ProductCategoryService $service)
-    {
-        return $this->render('website/layout/_navbar.html.twig', [
-            'categories' => $service->getList(),
-        ]);
-    }
-
     public function navtop(ProductCategoryService $service)
     {
         return $this->render('website/layout/_navtop.html.twig', [
